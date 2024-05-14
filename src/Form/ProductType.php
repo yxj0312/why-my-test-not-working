2f16d8514a4ab30ec99dd7f956663fcf44bc6502
yxj0312 <yanxiajun@hotmail.com>
2024-05-14 22:36:27 +0200

    add crud

---
+++ src/Form/ProductType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('quantity')
+            ->add('image')
+            ->add('is_featured')
+            ->add('is_active')
+            ->add('meta_title')
+            ->add('meta_description')
+            ->add('slug')
+            ->add('feature_image')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
---
+++ tests/Controller/ProductControllerTest.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProductControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private EntityManagerInterface $manager;
+    private EntityRepository $repository;
+    private string $path = '/product/';
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->manager = static::getContainer()->get('doctrine')->getManager();
+        $this->repository = $this->manager->getRepository(Product::class);
+
+        foreach ($this->repository->findAll() as $object) {
+            $this->manager->remove($object);
+        }
+
+        $this->manager->flush();
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Product index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+    }
+
+    public function testNew(): void
+    {
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'product[name]' => 'Testing',
+            'product[description]' => 'Testing',
+            'product[price]' => 'Testing',
+            'product[quantity]' => 'Testing',
+            'product[image]' => 'Testing',
+            'product[is_featured]' => 'Testing',
+            'product[is_active]' => 'Testing',
+            'product[meta_title]' => 'Testing',
+            'product[meta_description]' => 'Testing',
+            'product[slug]' => 'Testing',
+            'product[feature_image]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects($this->path);
+
+        self::assertSame(1, $this->repository->count([]));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Product();
+        $fixture->setName('My Title');
+        $fixture->setDescription('My Title');
+        $fixture->setPrice('My Title');
+        $fixture->setQuantity('My Title');
+        $fixture->setImage('My Title');
+        $fixture->setIs_featured('My Title');
+        $fixture->setIs_active('My Title');
+        $fixture->setMeta_title('My Title');
+        $fixture->setMeta_description('My Title');
+        $fixture->setSlug('My Title');
+        $fixture->setFeature_image('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Product');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Product();
+        $fixture->setName('Value');
+        $fixture->setDescription('Value');
+        $fixture->setPrice('Value');
+        $fixture->setQuantity('Value');
+        $fixture->setImage('Value');
+        $fixture->setIs_featured('Value');
+        $fixture->setIs_active('Value');
+        $fixture->setMeta_title('Value');
+        $fixture->setMeta_description('Value');
+        $fixture->setSlug('Value');
+        $fixture->setFeature_image('Value');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'product[name]' => 'Something New',
+            'product[description]' => 'Something New',
+            'product[price]' => 'Something New',
+            'product[quantity]' => 'Something New',
+            'product[image]' => 'Something New',
+            'product[is_featured]' => 'Something New',
+            'product[is_active]' => 'Something New',
+            'product[meta_title]' => 'Something New',
+            'product[meta_description]' => 'Something New',
+            'product[slug]' => 'Something New',
+            'product[feature_image]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/product/');
+
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getName());
+        self::assertSame('Something New', $fixture[0]->getDescription());
+        self::assertSame('Something New', $fixture[0]->getPrice());
+        self::assertSame('Something New', $fixture[0]->getQuantity());
+        self::assertSame('Something New', $fixture[0]->getImage());
+        self::assertSame('Something New', $fixture[0]->getIs_featured());
+        self::assertSame('Something New', $fixture[0]->getIs_active());
+        self::assertSame('Something New', $fixture[0]->getMeta_title());
+        self::assertSame('Something New', $fixture[0]->getMeta_description());
+        self::assertSame('Something New', $fixture[0]->getSlug());
+        self::assertSame('Something New', $fixture[0]->getFeature_image());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Product();
+        $fixture->setName('Value');
+        $fixture->setDescription('Value');
+        $fixture->setPrice('Value');
+        $fixture->setQuantity('Value');
+        $fixture->setImage('Value');
+        $fixture->setIs_featured('Value');
+        $fixture->setIs_active('Value');
+        $fixture->setMeta_title('Value');
+        $fixture->setMeta_description('Value');
+        $fixture->setSlug('Value');
+        $fixture->setFeature_image('Value');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertResponseRedirects('/product/');
+        self::assertSame(0, $this->repository->count([]));
+    }
+}
