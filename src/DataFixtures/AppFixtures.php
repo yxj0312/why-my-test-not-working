d54d50f452ba2b46ea24f17c91a01aa7ec4c66d4
yxj0312 <yanxiajun@hotmail.com>
2024-05-24 22:25:38 +0200

    update fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\ProductFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -14,6 +15,17 @@ class AppFixtures extends Fixture
     // }
     public function load(ObjectManager $manager): void
     {
-       $this->loadProjects($manager);
+        $this->loadProducts($manager);
+    }
+
+    private function loadProducts(ObjectManager $manager): void
+    {
+        // Use a factory to create 10 products with randomized data
+        $products = ProductFactory::create(10);
+
+        foreach ($products as $product) {
+            $manager->persist($product);
+        }
+        $manager->flush();
     }
 }
