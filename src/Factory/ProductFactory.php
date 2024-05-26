29f328fde90e0e15f3a865c1389345f7babc6596
yxj0312 <yanxiajun@hotmail.com>
2024-05-26 22:27:32 +0200

    Remove strange Provider warning

--- src/Factory/ProductFactory.php
+++ src/Factory/ProductFactory.php
@@ -12,19 +12,19 @@ class ProductFactory
         $faker = Factory::create();
         $products = [];
 
-        for ($i = 0; i < $count; $i++) {
+        for ($i = 0; $i < $count; $i++) {
             $product = new Product();
             $product->setName('Product ' . $i);
             $product->setDescription('Description for product ' . $i);
             $product->setPrice($faker->randomFloat(2, 10, 100));
             $product->setQuantity($faker->numberBetween(1, 100));
-            $product->setImage($faker->imageUrl());
+            $product->setImage('assert/images/image-test.png');
             $product->setIsFeatured($faker->boolean());
             $product->setIsActive($faker->boolean());
             $product->setMetaTitle($faker->sentence());
             $product->setMetaDescription($faker->sentence());
             $product->setSlug($faker->slug());
-            $product->setFeatureImage($faker->imageUrl());
+            $product->setFeatureImage('assert/images/image-test.png');
 
             // Add the product to the array
             $products[] = $product;
