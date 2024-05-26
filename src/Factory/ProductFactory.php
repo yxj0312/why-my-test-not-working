63662d44840e8625055dd21b220ccb205ed2a316
yxj0312 <yanxiajun@hotmail.com>
2024-05-26 22:18:15 +0200

    Update the Factory Method for Faker

--- src/Factory/ProductFactory.php
+++ src/Factory/ProductFactory.php
@@ -12,20 +12,18 @@ class ProductFactory
         $faker = Factory::create();
         $products = [];
 
-        for ($i = 0; $i < $count; $i++) {
+        for ($i = 0; i < $count; $i++) {
             $product = new Product();
-            // $product->setName($faker->word);
-            // $product->setDescription($faker->sentence);
             $product->setName('Product ' . $i);
             $product->setDescription('Description for product ' . $i);
             $product->setPrice($faker->randomFloat(2, 10, 100));
             $product->setQuantity($faker->numberBetween(1, 100));
             $product->setImage($faker->imageUrl());
-            $product->setIsFeatured($faker->boolean);
-            $product->setIsActive($faker->boolean);
-            $product->setMetaTitle($faker->sentence);
-            $product->setMetaDescription($faker->sentence);
-            $product->setSlug($faker->slug);
+            $product->setIsFeatured($faker->boolean());
+            $product->setIsActive($faker->boolean());
+            $product->setMetaTitle($faker->sentence());
+            $product->setMetaDescription($faker->sentence());
+            $product->setSlug($faker->slug());
             $product->setFeatureImage($faker->imageUrl());
 
             // Add the product to the array
