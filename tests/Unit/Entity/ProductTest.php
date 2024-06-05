b56cd7e0a69a0aefedaa181e655a769a93c536f6
yxj0312 <yanxiajun@hotmail.com>
2024-06-05 22:27:33 +0200

    add test

---
+++ tests/Unit/Entity/ProductTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
+class ProductTest extends TestCase
+{
+    public function testItWorks(): void
+    {
+        self:assertEquals(42, 42);
+    }
+}
