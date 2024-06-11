1517811292ab59e8b3198ca30a9d6013f2269518
yxj0312 <yanxiajun@hotmail.com>
2024-06-11 22:27:43 +0200

    test

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -17,6 +17,7 @@ class ProductController extends AbstractController
     #[Route('/', name: 'app_product_index', methods: ['GET'])]
     public function index(ProductRepository $productRepository): Response
     {
+        //test
         return $this->render('product/index.html.twig', [
             'products' => $productRepository->findAll(),
         ]);
